e783cfd8e653b469f8fbfabb493db6582ec43799
batman8056 <manimanish8056@gmail.com>
2025-01-02 19:07:44 +0530

    home page get  method working fine

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 import express from "express";
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+
+// Create __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
 
+// Set the views directory inside src
+app.set('views', path.join(__dirname, 'src', 'views'));
+
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+
 
 app.use(express.static("public"));
-app.get("/", (req, res) => {
-  res.render("index.ejs");
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
 
-// app.get("/about", (req, res) => {
-//   res.render("about.ejs");
+// app.get("/gallary", (req, res) => {
+//   res.render("gallary.ejs");
 // });
 
-// app.get("/contact", (req, res) => {
-//   res.render("contact.ejs");
+// app.get("/login", (req, res) => {
+//   res.render("login.ejs");
 // });
 
 app.listen(port, () => {
